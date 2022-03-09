9f29664f577d14306b51999be683d7369c368a91
Lotfi-Touil <lotfi.touil7@gmail.com>
2022-03-09 16:26:07 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PicturesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
