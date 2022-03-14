4942007bc6ee0da4ba14bd2d9314e5f48c9b302d
Lotfi-Touil <lotfi.touil7@gmail.com>
2022-03-14 16:36:35 +0100

    routingmodule about et contact

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchComponent } from './search/search.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot([
+      { path: 'search', component: SearchComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent }
+    ])
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: [],
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,24 @@ import { SearchComponent } from './search/search.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
